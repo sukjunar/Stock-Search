a07423cdd55144a5f182623c9f4180ad968a3049
Sukjune Hong <sukjune.hong@gmail.com>
2018-10-12 03:24:19 -0400

    Completes all the functionality and style

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Stock-Search
+
+## Welcome to the Stock Search ReadMe!
+
+This mobile-friendly Stock Search page pulls data from the iexTrading API. With the use of their API, a user can view the logo, CEO, current price, and the news of any publicly-traded company.
+
+## How to Use
+
+First thing to do is find the symbol of the company you want to find information about. Unfortunately, Stock Search currently does not find companies based on company name, but rather symbols.
+
+1. Input the symbol of the company of your choice in the input box to the top right.
+
+2. You can choose to save that symbol under "Favorites" if you wish.
+
+3. Any symbols that are registered with iexTrading will appear, either in the "Favorites" or the "History" bar.
+
+4. If you click on the button with your symbol, the information will generate below.
+
+5. If you wish to clear your "History" bar, you can do so by clicking the "Clear" button.
+
+## Any Questions?
+
+Feel free to contact Wayne Sukjune Hong at sukjune.hong@gmail.com.
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const stocksList = ['FB', 'AAPL', 'TSLA', 'GOOG'];
 const favesList = [];
+const validationList = [];
 
 const render = function () {
     $('.buttons').empty();
@@ -24,7 +25,9 @@ const faveRender = function () {
 const addButton = function (event) {
     event.preventDefault();
     const stock = $('#input').val().toUpperCase();
-    stocksList.push(stock);
+    if (validationList.includes(stock)) { 
+        stocksList.push(stock);
+    }
     $('#input').val('');
     render();
 }
@@ -38,8 +41,9 @@ const clearButton = function (event) {
 const favoriteButton = function (event) {
     event.preventDefault();
     const fave = $('#input').val().trim().toUpperCase();
-
-    favesList.push(fave);
+    if (validationList.includes(fave)) {
+        favesList.push(fave);
+    }
     $('#input').val('');
     faveRender();
 }
@@ -47,40 +51,58 @@ const favoriteButton = function (event) {
 const checkSymbol = function () {
     const symbolName = $(this).text();
     const queryURL = `https://api.iextrading.com/1.0/stock/${symbolName}/batch?types=quote,news,logo,company`;
+    const newsURL = `https://api.iextrading.com/1.0/stock/${symbolName}/news/last/10`;
     let = now = new Date();
 
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function (response) {
-        
+
         // LOGO
         $(".logo").html(`<img src="${response.logo.url}" style="width:250px"/>`);
-        
+
         // Company Name
         $(".companyName").html(`<h1>${response.company.companyName}</h1>`);
-        
+
         // CEO
         $(".ceoName").html(`<h2>CEO: ${response.company.CEO}</h2>`);
-        
+
         // Price
         $(".currentPrice").html(`<h2>Current Price: $${response.quote.latestPrice}</h2>`);
-        
+    })
+    $.ajax({
+        url: newsURL,
+        method: 'GET'
+    }).then(function (response) {
         // News
         $(".news").empty();
-        $(".news").html(`<h2>News</h2>`);
-        for (let i = 0; i < response.news.length; i++) {
-            $(".news").append(`<h3><a href="${response.news[i].url}">${response.news[i].headline}</a></h3><time>${response.news[i].datetime}</time><p>${response.news[i].summary}</p>`)
-        }
-    })
+        $(".news").html(`<h3>News</h3>`);
+        for (let i = 0; i < response.length; i++) {
+            $(".news").append(`<h4><a href="${response[i].url}" target="_blank">${response[i].headline}</a></h4><time>${response[i].datetime}</time><summary>${response[i].summary}</summary><br />`)
+        }    })
 }
 
-const randomFunction = function () {
+const pullSymbols = function () {
+    $.ajax({
+        url: "https://api.iextrading.com/1.0/ref-data/symbols",
+        method: 'GET'
+    }).then(function (response) {
+        for (let i = 0; i < response.length; i++) {
+            validationList.push(response[i].symbol)
+        }
+    })
+    render();
+}
+
+const consoleLog = function () {
     console.log("it works?")
 }
 
 render();
+pullSymbols();
 $('#submitButton').on('click', addButton);
 $('#clearButton').on('click', clearButton);
 $('#favoriteButton').on('click', favoriteButton);
 $('.buttons').on('click', '.stock', checkSymbol);
+$('.faveButtons').on('click', '.stock', checkSymbol);
